4c5439489288c07200594953907c045b892975fa
Garrett Schmahl <gschmahl@pm.me>
2021-03-31 12:53:18 -0700

    initialize repository

---
+++ gitInit.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "initialize repository"
+git branch -M main
+git remote add origin https://github.com/garrett-schmahl/project15-debugging-jquery.git
+git push -u origin main
